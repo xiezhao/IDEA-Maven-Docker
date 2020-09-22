a327a3f38aa9f598f51d5948ae2487f7ced2b1b8
rick <rick.xie@qiangyun.com>
2020-09-22 11:50:00 +0800

    first commit

---
+++ src/main/java/com/k8s/demo/controller/K8sController.java
@@ -0,0 +1,51 @@
+package com.k8s.demo.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.List;
+
+@RestController
+public class K8sController {
+
+    public static String mac = "";
+
+    @GetMapping("/mac")
+    public String getIps() throws Exception {
+        return getMAC();
+    }
+
+    public static String getMAC() throws SocketException {
+        if (!StringUtils.isEmpty(K8sController.mac)) return K8sController.mac;
+        java.util.Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+        while(en.hasMoreElements()){
+            NetworkInterface iface = en.nextElement();
+            List<InterfaceAddress> addrs = iface.getInterfaceAddresses();
+            StringBuilder sb = new StringBuilder();
+            for(InterfaceAddress addr : addrs) {
+                InetAddress  ip = addr.getAddress();
+                NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+                if(network==null){
+                    continue;
+                } else {
+                    byte[] mac = network.getHardwareAddress();
+                    if(mac==null) {
+                        continue;
+                    } else{
+                        for (int i = 0; i < mac.length; i++) {
+                            sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                        }
+                        K8sController.mac = sb.toString();
+                        break;
+                    }
+                }
+            }
+        }
+        return K8sController.mac;
+    }
+}
